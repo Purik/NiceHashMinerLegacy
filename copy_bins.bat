02e51b4d0c0a748ffc992fcaaaefb49512660382
Pavel_Minenkov <minikspb@gmail.com>
2018-02-28 18:03:06 +0300

    adv bins copiing scripts

---
+++ copy_bins.bat
@@ -0,0 +1,8 @@
+@echo off
+set CFG=%1
+
+echo Cnfiguration: "%CFG%"
+
+xcopy AMDOpenCLDeviceDetection\%CFG% %CFG% /Y /EXCLUDE:ignorefiles.txt
+copy setcpuaff\%CFG%\setcpuaff.vcxproj.exe setcpuaff\%CFG%\setcpuaff.exe
+xcopy setcpuaff\%CFG% %CFG% /Y /EXCLUDE:ignorefiles.txt
