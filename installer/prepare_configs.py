281c05178267bc9bb53984a356eabffd43e21cfe
Pavel_Minenkov <minikspb@gmail.com>
2018-03-20 15:50:09 +0300

    patch meta

--- installer/prepare_configs.py
+++ installer/prepare_configs.py
@@ -5,6 +5,9 @@ import os
 import glob
 import datetime
 import shutil
+import re
+
+import requests
 
 
 logging.basicConfig(format='%(levelname)s:  %(pathname)s \n\t%(message)s', level=logging.DEBUG, stream=sys.stdout)
@@ -15,10 +18,12 @@ BASE_DIR = os.path.dirname(__file__)
 parser = argparse.ArgumentParser()
 parser.add_argument('--version', type=str, required=True, help='Version.')
 parser.add_argument('--bin_dir', type=str, required=True, help='Binary Directories.')
+parser.add_argument('--secret', help='Secret token for server software version update')
 args = parser.parse_args()
 
 cur_date = datetime.datetime.now().isoformat().split('T')[0]
 
+# 1. Patch configs and meta files
 for file in glob.glob('config/*.xml') + glob.glob('packages/com.cryptominer.app/meta/*.xml'):
     with open(file, 'r+') as f:
         logging.debug(f'Replace content file: "{file}"')
@@ -31,6 +36,33 @@ for file in glob.glob('config/*.xml') + glob.glob('packages/com.cryptominer.app/
     pass
 
 
+# 2. Patch Assembly info
+URL = 'https://freezone.name/ajax/nicehash-software-version'
+assembly_file = os.path.realpath(os.path.join(BASE_DIR, '../NiceHashMiner/Properties/AssemblyInfo.cs', ))
+with open(assembly_file, 'r+') as f:
+    content = f.read()
+
+    search = re.search('AssemblyVersion(\(")\d.\d.\d.\d"\)', content).group(0)
+    orig_version = re.search('\d.\d.\d.\d', search).group(0)
+    resp = requests.post(URL, dict(version=orig_version, token=args.secret))
+
+    find = 'AssemblyVersion(.)*'
+    replace = f'AssemblyVersion("{args.version}")'
+    content = re.sub(find, replace, content)
+
+    find = 'AssemblyFileVersion(.)*'
+    replace = f'AssemblyFileVersion("{args.version}")'
+    content = re.sub(find, replace, content)
+
+    content = re.sub('NiceHashMinerLegacy', 'CryptoMiner', content)
+    content = re.sub('NiceHash', 'CryptoMiner', content)
+
+    f.seek(0)
+    f.truncate(0)
+    f.write(content)
+
+exit()
+# 3. Copy bin directory
 if not os.path.isdir(args.bin_dir):
     raise ValueError(f'Directory "{str(args.bin_dir)}" does not exists!')
 
